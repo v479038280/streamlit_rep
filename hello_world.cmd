67d298e7d445063aeb493ce728580ee14cb0489c
v479038280 <v479038280@foxmail.com>
2023-08-06 16:20:49 +0800

    Add files via upload

---
+++ hello_world.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'hello_world.exe' with Python DLL being found.
+set PATH=d:\program files\python;%PATH%
+set PYTHONHOME=D:\Program Files\python
+"%~dp0.\hello_world.exe" %*
